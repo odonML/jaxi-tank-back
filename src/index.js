a9987ff972e42e1c4d9ba6366d51e188b19870fd
Odon ML <30378361+odonML@users.noreply.github.com>
2021-06-23 08:12:34 -0500

    Revert "0.1v addTask"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema";
 import express from "express";
-import {connect } from "./database"
 
 const port = 3001;
 
 const app = express();
-connect();
 
 app.get('/', (req, res) => {add
     res.json({
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,7 @@
-import Task from "./models/task"
 export const resolvers = {
     Query: {
         hello: () => {
             return 'hola';
-        }
-    },
-    Mutation: {
-        createTask: async (_,{input}) => {
-            const newTask = new Task(input);
-            await newTask.save();
-            return newTask;
         }
     }
 }
