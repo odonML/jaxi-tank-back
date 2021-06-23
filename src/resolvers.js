70c81431135e9be47944b30ce2adf61002f66a7c
Odon_ML <odon@integriapp.com>
2021-06-23 10:25:29 -0500

    0.1v crudUser

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,29 @@
-import Task from "./models/task"
+import Task from "./models/task";
+import User from "./models/user";
 export const resolvers = {
     Query: {
+        users: async () =>{
+            return await User.find();
+        },
         tasks: async () =>{
             return await Task.find();
         }
     },
     Mutation: {
+        createUser: async (_, {input}) =>{
+            const newUser = new User(input);
+            return await newUser.save();
+        },
+        deleteUser: async (_, {_id}) =>{
+            return await User.findByIdAndDelete(_id);
+        },
+        updateUser: async (_, {_id, input}) =>{
+            return await User.findByIdAndUpdate(_id,input,{new: true});
+        },
+
         createTask: async (_,{input}) => {
             const newTask = new Task(input);
-            await newTask.save();
-            return newTask;
+            return await newTask.save();
         },
         deleteTask: async (_, {_id}) =>{
             return await Task.findByIdAndDelete(_id);
