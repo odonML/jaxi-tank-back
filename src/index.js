c0192b361c36100915e7a568f11790ccb0aed167
Odon_ML <odon@integriapp.com>
2021-06-22 23:03:05 -0500

    0.1v node with graphql

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
 import express from "express";
 
 const port = 3001;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {add
     res.json({
         menssge: 'hola mundo xD'
     })
 })
 
-
-
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}),);
 
 app.listen(port, () => {console.log(`server on port ${port}`)})
